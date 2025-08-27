1b1efa703ce07067b1ad62a719337a600e80b2e2
Bruna Lais Lindner <brunalaislindner@outlook.com>
2025-08-26 21:00:36 -0300

    melhorias na classe cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -9,14 +9,28 @@ namespace POO_e_Encapsulamento.Models
     {
         public Cliente(string nome, string cpf, string telefone)
         {
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                throw new ArgumentException("O nome do cliente não pode ser vazio", nameof(nome));
+            }
+            if (string.IsNullOrWhiteSpace(cpf))
+            {
+                throw new ArgumentException("O CPF não pode ser vazio", nameof(cpf));
+            }
+
             Nome = nome;
             Cpf = cpf;
             Telefone = telefone;
         }
 
         public string Nome { get; set; }
-            
-        public string Cpf { get; set; }
+
+        public string Cpf { get; private set; }
         public string Telefone { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} | CPF: {Cpf} | Telefone: {Telefone}";
+        }
     }
 }
